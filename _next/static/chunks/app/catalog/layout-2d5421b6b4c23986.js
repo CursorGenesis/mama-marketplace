324aa5b9b6be62e56dbe2807caf4a5eac65acfa5
alla <alla@mail.kg>
2026-04-20 14:51:36 +0600

    Ручной деплой с правильным basePath

--- _next/static/chunks/app/catalog/layout-2d5421b6b4c23986.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1418,7409,8459,4738,2993,2167,6841,2339,834,6535],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
