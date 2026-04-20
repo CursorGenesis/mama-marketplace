a5c14244e244b1c72c2e61544476736b82003d45
alla <alla@mail.kg>
2026-04-20 14:35:21 +0600

    Ручной деплой с правильным basePath

---
+++ _next/static/chunks/app/pricing/layout-86dfdc85e9f199fd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[834,7409,2993,1418,8459,2339,2167,4738,6535,6841],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
