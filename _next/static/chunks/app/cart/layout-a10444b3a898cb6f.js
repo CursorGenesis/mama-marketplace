d952b78a5b9d7c77e3596acde6a986ec41381bc0
alla <alla@mail.kg>
2026-04-21 10:18:47 +0600

    Деплой: how-it-works

---
+++ _next/static/chunks/app/cart/layout-a10444b3a898cb6f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2993,7409,6841,8459,1418,4738,2339,2167,834,6535],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
