04ec4400bf2daa342e0cafa8f56ed27efc642694
alla <alla@mail.kg>
2026-04-22 22:20:51 +0600

    deploy: ротация Telegram-токена после утечки

--- _next/static/chunks/app/about/layout-5d0c6d4235f0e436.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6841,7409,1418,4738,2339,2167,2993,8459,6535,834],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
