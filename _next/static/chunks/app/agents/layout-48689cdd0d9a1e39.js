809284d8a5360a7252cf0c06f397fb87e9a66c1b
alla <alla@mail.kg>
2026-04-22 10:56:32 +0600

    Улучшен профиль-дропдаун для мобильной версии

--- _next/static/chunks/app/agents/layout-48689cdd0d9a1e39.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8459,7409,6841,1418,2339,2167,4738,2993,834,6535],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
