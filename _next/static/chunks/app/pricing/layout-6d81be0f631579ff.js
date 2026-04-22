0152faacfc5e2fd3253845b0cdd3ae64076b2ecb
alla <alla@mail.kg>
2026-04-22 12:14:21 +0600

    Переставлен порядок блоков на главной + удалена карта

--- _next/static/chunks/app/pricing/layout-6d81be0f631579ff.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[834,7409,8459,4738,1418,6841,2167,2993,6535,2339],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
