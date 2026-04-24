5652d27b396930e54cb4332c0a4d908bc4509f88
alla <alla@mail.kg>
2026-04-24 17:08:21 +0600

    deploy: админский email на alla.albertovna1@gmail.com

---
+++ out/_next/static/chunks/app/pricing/layout-8ce0499b1376e6c5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[834,7409,4738,6841,8459,2993,2167,1418,2339,6535],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
