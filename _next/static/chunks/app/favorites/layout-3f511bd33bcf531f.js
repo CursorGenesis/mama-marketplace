d4301fd0ee02540f503984a99bbb1e490095ab0f
alla <alla@mail.kg>
2026-04-24 19:38:36 +0600

    deploy: пакет A критичных багфиксов — монетки и комиссии при received, XSS защита, жёсткая дата розыгрыша

---
+++ _next/static/chunks/app/favorites/layout-3f511bd33bcf531f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2339,7409,8459,1418,4738,834,2993,2167,6535,6841],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
