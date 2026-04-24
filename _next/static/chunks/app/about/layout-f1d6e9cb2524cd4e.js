37e96993892b9a3723f053aab7c3338ae8f6b408
alla <alla@mail.kg>
2026-04-24 14:24:47 +0600

    deploy: розыгрыш — порог 100 монеток/мес + шкала билетов 1/3/5/10

---
+++ _next/static/chunks/app/about/layout-f1d6e9cb2524cd4e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6841,7409,8459,1418,2993,2339,2167,834,4738,6535],{3521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
